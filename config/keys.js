87660c3679be8a68acea8f345579b21e617963e2
Phillip Bindeman <9490743+bindeman@users.noreply.github.com>
2020-10-19 23:14:09 -0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,2 @@
 module.exports = {
-    mongoURI: "mongodb+srv://rootAdmin:36ZFioRjcTFQQA8n@globalcred.0nxk8.mongodb.net/CREDEvents?retryWrites=true&w=majority"
-}
+    mongoURI: process.env.DATABASE
