17c1f0b82c8c51e17669c29b63431831abffe431
Phillip Bindeman <9490743+bindeman@users.noreply.github.com>
2020-11-24 23:52:06 -0800

    Fix import

--- frontend/src/main/events/eventTypes/Events.js
+++ frontend/src/main/events/eventTypes/Events.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
